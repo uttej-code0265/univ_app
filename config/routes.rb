0960173a790b710609fe2fee5c520f7f1ed2b4cd
Uttej Kumar <uttejkumarponugoti@gmail.com>
2020-09-10 16:49:02 +0530

    complete application

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'courses#index'
+  get 'about',to:'pages#about'
+  get 'courses/new',to:'courses#new'
+  resources :students,except: [:destroy]
+  get 'login',to:'sessions#new'
+  post 'login',to:'sessions#create'
+  delete 'logout',to:'sessions#destroy'
+  post 'course_enroll',to:'student_courses#create'
+end
